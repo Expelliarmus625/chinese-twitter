3bff90ad6a73aec3fb4862735313f1e3c868eea1
Ojas Barpande <barpande.ojas@gmail.com>
2020-04-24 22:53:01 +0530

    Added ajax support

--- chinatwitter/tweets/views.py
+++ chinatwitter/tweets/views.py
@@ -44,6 +44,9 @@ def tweet_create_view(request):
     if form.is_valid():
         obj = form.save(commit=False)
         obj.save()
+        if request.is_ajax():
+            return JsonResponse({}, status=201)
+
         if next_url is not None and is_safe_url(next_url, settings.ALLOWED_HOSTS):
             return redirect(next_url)
         form = TweetForm()
